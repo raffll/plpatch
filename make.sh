94342a4023cb1ba46b08b3a12433121f344400f1
fierenea <rafal.wierzchos@gmail.com>
2021-10-29 08:05:01 +0200

    w

--- make.sh
+++ make.sh
@@ -16,4 +16,4 @@ mv "Morrowind.CREATED.esm" "Morrowind [plpatch].esm"
 mv "Tribunal.CREATED.esm" "Tribunal [plpatch].esm"
 mv "Bloodmoon.CREATED.esm" "Bloodmoon [plpatch].esm"
 
-zip "$NAME.zip" "Morrowind [plpatch].esm" "Tribunal [plpatch].esm" "Bloodmoon [plpatch].esm" "plpatch.bat" "README.odt" "yampt.exe" "CHANGELOG.htm" "ENtoPL_plpatch_${VER}_DIAL.xml" "ENtoPL_plpatch_${VER}_CELL.xml" "ENtoPL_plpatch_${VER}.xml"
+zip "$NAME.zip" "Morrowind [plpatch].esm" "Tribunal [plpatch].esm" "Bloodmoon [plpatch].esm" "plpatch.bat" "README.pdf" "yampt.exe" "CHANGELOG.htm" "ENtoPL_plpatch_${VER}_DIAL.xml" "ENtoPL_plpatch_${VER}_CELL.xml" "ENtoPL_plpatch_${VER}.xml"
--- plpatch.bat
+++ plpatch.bat
@@ -6,6 +6,6 @@ set ESM=
 for %%f in ("*.esp", "*.esm") do set ESM=!ESM! "%%f"
 set ESM=%ESM:~1%
 
-yampt.exe --convert -f %ESM% -d "ENtoPL_plpatch_CELL.xml" "ENtoPL_plpatch_DIAL.xml"
+yampt.exe --convert -f %ESM% -d "ENtoPL_plpatch_3.0_CELL.xml" "ENtoPL_plpatch_3.0_DIAL.xml"
 
 pause
