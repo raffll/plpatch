e8e42e92b0b23e65390fd4c0a3da3138a4070851
raffll <rafal.wierzchos@gmail.com>
2022-04-13 09:57:12 +0200

    wip

--- 3.1/make.sh
+++ 3.1/make.sh
@@ -16,6 +16,4 @@ mv "Morrowind.CREATED.esm" "Morrowind [plpatch].esm"
 mv "Tribunal.CREATED.esm" "Tribunal [plpatch].esm"
 mv "Bloodmoon.CREATED.esm" "Bloodmoon [plpatch].esm"
 
-zip "$NAME.zip" "Morrowind [plpatch].esm" "Tribunal [plpatch].esm" "Bloodmoon [plpatch].esm" "plpatch.bat" "README.pdf" "yampt.exe" "CHANGELOG.htm" "ENtoPL_plpatch_DIAL.xml" "ENtoPL_plpatch_CELL.xml" "ENtoPL_plpatch.xml"
-
-rm "Morrowind [plpatch].esm" "Tribunal [plpatch].esm" "Bloodmoon [plpatch].esm" "yampt.log"
+zip "$NAME.zip" "Morrowind [plpatch].esm" "Tribunal [plpatch].esm" "Bloodmoon [plpatch].esm" "plpatch.bat" "README.pdf" "yampt.exe" "CHANGES.htm" "CHANGES_CELL.htm" "CHANGES_DIAL.htm" "ENtoPL_plpatch_DIAL.xml" "ENtoPL_plpatch_CELL.xml" "ENtoPL_plpatch.xml"
